33a0d6e8e79aad53aaabbbd4db48ad1d07c6a064
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-05-19 23:12:09 +0200

    docs: update documentation

--- R/ds.useDatadist.R
+++ R/ds.useDatadist.R
@@ -17,23 +17,22 @@
 #'   
 #'   # connecting to the Opal servers
 #'   builder <- DSI::newDSLoginBuilder()
-#'   builder$append(server = "study1", 
-#'                 url = "http://192.168.56.100:8080/", 
-#'                 user = "administrator", password = "datashield_test&", 
-#'                 table = "SURVIVAL.EXPAND_NO_MISSING1", driver = "OpalDriver")
+#'   builder$append(server = "study1",
+#'                  url = "http://192.168.56.100:8080/",
+#'                  user = "administrator",
+#'                  password = "datashield_test&",
+#'                  table = "SURVIVAL.EXPAND_WITH_MISSING1",
+#'                  driver = "OpalDriver")
 #'   logindata <- builder$build()
-#'   
 #'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D")
-#'   
-#'   # Create datadist object
-#'   ds.datadist(data = "D", objectname = "dd")
-#'   
+#'   ds.mice(data = 'D', m = 5, method = 'rf', newobj_df = 'D2', seed = 'fixed', newobj_mids = "imputed_mids")
+#'
+#'   ds.datadist(data = 'D2.1', adjust_to = list(age.60 = 'min'))
+#'
 #'   # Set the datadist object as the global option
-#'   ds.useDatadist(datadist = "dd")
-#'   
-#'   # Now rms plotting functions will use this datadist object
-#'   
-#'   # Clear the Datashield R sessions and logout
+#'   ds.useDatadist(datadist = "datadist_D2.1")
+#'
+#'   # When finished, clear the session and logout
 #'   datashield.logout(connections)
 #' }
 #'
